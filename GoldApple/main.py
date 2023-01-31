213b394955c8c432782ddbf8ef0aee1cae5a261e
FireFading <svetl.rudewa2014@yandex.ru>
2023-01-31 20:46:18 +0300

    add check time after sending code

--- GoldApple/main.py
+++ GoldApple/main.py
@@ -2,10 +2,11 @@ from common.base import Base
 from common.env import phone
 from common.locators import GoldAppleLocators
 from selenium.webdriver.common.keys import Keys
-from common.settings import timeout
+from common.settings import timeout, max_wait_time, code_expired_time
 
 from GoldApple.settings import Urls
 import time
+from datetime import datetime
 
 
 class GoldApple(Base):
@@ -14,10 +15,12 @@ class GoldApple(Base):
         self.send_values(locator=GoldAppleLocators.PHONE_INPUT, value=phone)
         code = input("Input confirmation code: ")
         self.send_values(locator=GoldAppleLocators.CODE_INPUT, value=code)
-        while self.is_element_present(locator=GoldAppleLocators.WRONG_CODE_MESSAGE):
+        code_expired = datetime.now() +  datetime.timedelta(minutes=code_expired_time)
+        while self.is_element_present(locator=GoldAppleLocators.WRONG_CODE_MESSAGE) or datetime.now() >= code_expired:
             self.resend_code()
             code = input("Input confirmation code: ")
             self.send_values(locator=GoldAppleLocators.CODE_INPUT, value=code)
+            code_expired = datetime.now() +  datetime.timedelta(minutes=code_expired_time)
 
     def city_confirm(self) -> None:
         self.find_and_click_element(locator=GoldAppleLocators.CITY_CONFIRM)
@@ -29,7 +32,7 @@ class GoldApple(Base):
         self.find_and_click_element(locator=GoldAppleLocators.SEARCH_BUTTON)
         while not self.is_element_present(locator=GoldAppleLocators.SEARCH_INPUT):
             self.browser.implicitly_wait(timeout)
-        search_input = self.browser.find_element(GoldAppleLocators.SEARCH_INPUT)
+        search_input = self.browser.find_element(*GoldAppleLocators.SEARCH_INPUT)
         search_input.send_keys(query)
         search_input.send_keys(Keys.ENTER)
         time.sleep(400)
--- common/settings.py
+++ common/settings.py
@@ -1,8 +1,9 @@
 from random import randint
 
 
-def timeout():
+def timeout():  # seconds
     return randint(a=3, b=10)
 
 
-max_wait_time = 40
+max_wait_time = 40  # second
+code_expired_time = 1  # minutes
